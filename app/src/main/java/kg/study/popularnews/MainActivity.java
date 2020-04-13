dd7d3e7d37dc956e058117137ae840e662952f9a
jy1dyz <jyldyz.kgz@gmail.com>
2020-04-13 21:46:16 +0600

    added swipeRefreshLayout, errorLayout, NewsDetailActivity, Share option

--- app/src/main/java/kg/study/popularnews/MainActivity.java
+++ app/src/main/java/kg/study/popularnews/MainActivity.java
@@ -1,17 +1,30 @@
 package kg.study.popularnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
+import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
+import android.media.tv.TvContentRating;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
 import androidx.appcompat.widget.SearchView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -25,7 +38,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     public static final String API_KEY = "3125f0e16a2e44babcdd15b80775d54c";
     private RecyclerView recyclerView;
@@ -33,11 +46,23 @@ public class MainActivity extends AppCompatActivity {
     private List<Article> articles = new ArrayList<>();
     private Adapter adapter;
     private String TAG = MainActivity.class.getSimpleName();
+    private TextView topHeadLine;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RelativeLayout errorLayout;
+    private ImageView errorImageView;
+    private TextView errorTitle, errorMessage;
+    private Button btnRetry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+
+        topHeadLine = findViewById(R.id.tv_headlines);
 
         recyclerView = findViewById(R.id.recycler_view);
         layoutManager = new LinearLayoutManager(this);
@@ -45,18 +70,29 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setNestedScrollingEnabled(false);
 
-        loadJson("");
+        errorLayout = findViewById(R.id.errorLayout);
+        errorImageView = findViewById(R.id.errorImage);
+        errorTitle = findViewById(R.id.errorTitle);
+        errorMessage = findViewById(R.id.errorMessage);
+        btnRetry = findViewById(R.id.btn_retry);
 
+
+        //loadJson("");
+        onLoadingSwipeRefresh("");
     }
 
     public void loadJson(final String keyword) {
+
+        errorLayout.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(true);
+
         ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
         String country = Utils.getCountry();
 
         Call<News> call;
 
         if (keyword.length() > 0) {
-            call = apiInterface.getNewssearch(keyword,  API_KEY);
+            call = apiInterface.getNewssearch(keyword, API_KEY);
         } else {
             call = apiInterface.getNews(country, API_KEY);
         }
@@ -74,15 +110,71 @@ public class MainActivity extends AppCompatActivity {
                     recyclerView.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
 
+                    initListener();
+
+                    topHeadLine.setVisibility(View.VISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
 
                 } else {
-                    Toast.makeText(MainActivity.this, "No Results", Toast.LENGTH_SHORT).show();
+                    topHeadLine.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+
+                    String errorCode;
+                    switch (response.code()) {
+                        case 404:
+                            errorCode = "404 not found";
+                            break;
+                        case 500:
+                            errorCode = "500 server broken";
+                            break;
+                        default:
+                            errorCode = "unknown error";
+                            break;
+                    }
+                    showErrorMessage(
+                            R.drawable.ic_mood_bad,
+                            "No Result",
+                            "Please try again!\n" +
+                                    errorCode);
                 }
             }
 
             @Override
             public void onFailure(Call<News> call, Throwable t) {
+                topHeadLine.setVisibility(View.INVISIBLE);
+                swipeRefreshLayout.setRefreshing(false);
 
+                showErrorMessage(
+                        R.drawable.ic_mood_bad,
+                        "Oops...",
+                        "Network failure, please try again!\n" +
+                                t.toString());
+            }
+        });
+    }
+
+    private void initListener() {
+        adapter.setOnItemClickListener(new Adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+                ImageView imageView = view.findViewById(R.id.image);
+                Intent intent = new Intent(MainActivity.this, NewsDetailActivity.class);
+
+                Article article = articles.get(position);
+                intent.putExtra("url", article.getUrl());
+                intent.putExtra("title", article.getTitle());
+                intent.putExtra("img", article.getUrlToImage());
+                intent.putExtra("date", article.getPublishedAt());
+                intent.putExtra("source", article.getSource().getName());
+                intent.putExtra("author", article.getAuthor());
+
+                Pair<View, String> pair = Pair.create(imageView, ViewCompat.getTransitionName(imageView));
+                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        MainActivity.this, pair
+                );
+
+                startActivity(intent, optionsCompat.toBundle());
             }
         });
     }
@@ -101,14 +193,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 if (query.length() > 2) {
-                    loadJson(query);
+                    onLoadingSwipeRefresh(query);
                 }
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                loadJson(newText);
                 return false;
             }
         });
@@ -117,4 +208,36 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onRefresh() {
+        loadJson("");
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword) {
+        swipeRefreshLayout.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        loadJson(keyword);
+                    }
+                }
+        );
+    }
+
+    private void showErrorMessage(int imageView, String title, String message) {
+        if (errorLayout.getVisibility() == View.GONE) {
+            errorLayout.setVisibility(View.VISIBLE);
+        }
+
+        errorImageView.setImageResource(imageView);
+        errorTitle.setText(title);
+        errorMessage.setText(message);
+
+        btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoadingSwipeRefresh("");
+            }
+        });
+    }
 }
